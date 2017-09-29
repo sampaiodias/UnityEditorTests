d7852e43509ac655bf9d9f024f255659bea04fb4
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2017-09-29 13:00:37 -0300

    First Commit

---
+++ Assets/Scripts/Editor/CreateGameObject.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateGameObject : EditorWindow {
+
+    bool addScript;
+    int value;
+
+    bool specialMaterial;
+    Color color;
+
+    string prefabPath = "Prefabs/CapsulePrefabExample.prefab";
+
+    string myName = "NewGameObject";
+    string shader = "Standard";
+
+    [MenuItem("Window/EditorTests/GameObject Creator")]
+    public static void ShowWindow()
+    {
+        GetWindow<CreateGameObject>("Obj Creator");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Space(10);
+        GUILayout.Label("Properties", EditorStyles.boldLabel);
+        myName = EditorGUILayout.TextField("Object Name ", myName);
+        GUILayout.Space(10);
+
+        specialMaterial = EditorGUILayout.BeginToggleGroup("Change Material?", specialMaterial);
+        shader = EditorGUILayout.TextField("Shader Name ", shader);
+        color = EditorGUILayout.ColorField("Object Color ", color);
+        EditorGUILayout.EndToggleGroup();
+        GUILayout.Space(10);
+
+        addScript = EditorGUILayout.BeginToggleGroup("Add SimpleScript?", addScript);
+        value = EditorGUILayout.IntSlider("Value ", value, 0, 10);
+        EditorGUILayout.EndToggleGroup();
+
+        GUILayout.Space(20);
+
+        GUILayout.Label("Create From Primitive Shape", EditorStyles.boldLabel);
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Capsule"))
+        {
+            Create("Capsule");
+        }
+
+        if (GUILayout.Button("Cube"))
+        {
+            Create("Cube");
+        }
+
+        if (GUILayout.Button("Sphere"))
+        {
+            Create("Sphere");
+        }
+        if (GUILayout.Button("Cylinder"))
+        {
+            Create("Cylinder");
+        }
+
+        if (GUILayout.Button("Plane"))
+        {
+            Create("Plane");
+        }
+
+        if (GUILayout.Button("Quad"))
+        {
+            Create("Quad");
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(30);
+        GUILayout.Label("Instantiate From Prefab", EditorStyles.boldLabel);
+        prefabPath = EditorGUILayout.TextField("Prefab Path ", prefabPath);
+        GUILayout.Space(10);
+        if (GUILayout.Button("Prefab"))
+        {
+            Create("Prefab");
+        }
+
+        GUILayout.Space(30);
+        GUILayout.Label("Game Object Creator - By Lucas Sampaio Dias", EditorStyles.centeredGreyMiniLabel);
+    }
+
+    void Create(string type)
+    {
+        GameObject obj;
+        switch (type)
+        {
+            default:
+                obj = new GameObject();
+                break;
+            case ("Cube"):
+                obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                break;
+            case ("Capsule"):
+                obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                break;
+            case ("Sphere"):
+                obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                break;
+            case ("Cylinder"):
+                obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                break;
+            case ("Plane"):
+                obj = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                break;
+            case ("Quad"):
+                obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                break;
+            case ("Prefab"):
+                GameObject prefab = (GameObject)AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject));
+                //obj = (GameObject)Instantiate(prefab);
+                obj = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+                break;
+        }
+
+        obj.name = myName;
+
+        if (addScript)
+        {
+            obj.AddComponent<SimpleScript>();
+            SimpleScript script = obj.GetComponent<SimpleScript>();
+            script.myValue = value;
+        }
+
+        if (specialMaterial)
+        {
+            Material material = new Material(Shader.Find(shader));
+            AssetDatabase.CreateAsset(material, "Assets/Materials/" + myName + ".mat");          
+
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = material;
+                renderer.sharedMaterial.color = color;
+            }
+        }        
+    }
+}
---
+++ Assets/Scripts/Editor/WindowTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class WindowTest : EditorWindow {
+
+    string myString = "Exemplo";
+    bool groupEnabled;
+    bool subgroupEnabled;
+    bool myBool = true;
+    int myIntSliderValue = 0;
+    float myFloat = 1.23f;
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/EditorTests/My Window")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        WindowTest window = (WindowTest)EditorWindow.GetWindow(typeof(WindowTest));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Polígono", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Nome", myString);
+        
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Inimigo", groupEnabled);
+        myIntSliderValue = EditorGUILayout.IntSlider("Tipo", myIntSliderValue, 0, 10);
+        subgroupEnabled = EditorGUILayout.BeginToggleGroup("Dificuldade Padrão", subgroupEnabled);
+        myFloat = EditorGUILayout.Slider("Dificuldade", myFloat, 0, 1);
+        EditorGUILayout.EndToggleGroup();
+        
+        if (GUILayout.Button("Aplicar"))
+        {
+            Debug.Log("Aplicado");
+            GameObject x = new GameObject(myString);
+            Undo.RegisterCreatedObjectUndo(x, myString);
+        }
+        EditorGUILayout.EndToggleGroup();
+        
+    }
+}
